806b4a9537256e56741f3edfa33eccfd5084852d
Zun Liang <zliang0331@gmail.com>
2023-02-06 15:31:36 +0800

    updated faq.js

--- faq.js
+++ faq.js
@@ -4,11 +4,19 @@ const arrows = document.querySelectorAll(".arrow")
 
 for (let i = 0; i < questions.length; i++) {
     questions[i].addEventListener("click", function toggle(){
+
         if (answers[i].style.display == "none" || answers[i].style.display == "") {
             answers[i].style.display = "block";
             arrows[i].style.rotate = "-180deg";
             questions[i].style.color = "var(--grayish-blue-dark)";
             questions[i].style.fontWeight = "700";
+            for (let j = 0; j < questions.length; j++) {
+                if(i !== j) {
+                    answers[j].style.display = "none";
+                    arrows[j].style.rotate = "none";
+                    questions[j].style.fontWeight = "400";
+                }
+            }
         } else {
             answers[i].style.display = "none";
             arrows[i].style.rotate = "none";
